6bdc184cfda786aa1e9a44874fa6e4dc88bc010b
Michael Kim <michael.kim.200518@gmail.com>
2024-05-09 13:35:34 +0900

    Fix issue: Variables That Could Be Declared as Immutable

--- packages/contracts/contracts/MultiSigWallet.sol
+++ packages/contracts/contracts/MultiSigWallet.sol
@@ -33,8 +33,8 @@ contract MultiSigWallet is ERC165, IMultiSigWallet {
      */
     string public override name;
     string public override description;
-    address public override creator;
-    uint256 public override createdTime;
+    address public immutable override creator;
+    uint256 public immutable override createdTime;
 
     mapping(uint256 => Transaction) internal transactions;
     mapping(uint256 => mapping(address => bool)) internal confirmations;
@@ -42,7 +42,7 @@ contract MultiSigWallet is ERC165, IMultiSigWallet {
     address[] internal members;
     uint256 internal required;
     uint256 internal transactionCount;
-    address internal factoryAddress;
+    address internal immutable factoryAddress;
 
     /*
      *  Modifiers
